bd9c6c0c508f372752dc30483b3423f7f78a94d5
Alexis <alexis@cvofhope.com>
2019-04-15 16:44:52 +1000

    import jhead procs

--- src/jhead.nim
+++ src/jhead.nim
@@ -1,2 +1,13 @@
-# Hello Nim!
-echo "Hello, World!"
+import os
+import jheadpkg/jheadprocs
+
+proc main():void = 
+  echo EnsurePathExists("/Users/lhope/dev/g.jpg")
+  # let argc:cint = cint(2);
+  # let args:array[2, string] = ["abc", "foo"]
+  # let argv = allocCStringArray(args)
+  # discard JheadMain(argc, argv)
+  # deallocCStringArray(argv)
+
+if isMainModule:
+  main()
