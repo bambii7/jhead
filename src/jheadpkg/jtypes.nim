30c34400cc5353b1b417bab5b44970a30416ed9e
Alexis <alexis@cvofhope.com>
2019-05-21 17:41:35 +1000

    update jtypes with limited properties

--- src/jheadpkg/jtypes.nim
+++ src/jheadpkg/jtypes.nim
@@ -19,50 +19,50 @@ type
     resolutionUnits*: byte
     xDensity*: byte
     yDensity*: byte
-  GpsInfo = object
-    lat: char
-    long: char
-    alt: char
+  # GpsInfo* = object
+  #   lat*: char
+  #   long*: char
+  #   alt*: char
   ImageInfo* = object
-    fileName: char
-    fileDateTime: DateTime
-    jfifHeaderL: JifiHeader
-    fileSize: uint
-    cameraMake: char
-    cameraModel: char
-    dateTime: char
-    height: uint
-    width: uint
-    orientation: int
-    isColor: int
-    process: int
-    flashUsed: int
-    focalLength: float
-    exposureTime: float
-    apertureFNumer: float
-    distance: float
-    ccdWidth: float
-    exposureBias: float
-    digitalZoomRatio: float
-    focalLength35mmEquiv: int
-    whiteBalance: int
-    masteringMode: int
-    exposureProgram: int
-    exposureMode:int
-    isoEquivalent: int
-    lightSource: int
-    distanceRange: int
-    xResolution: float
-    yResolution: float
-    resolutionUnit: int
-    comments: string
-    commentWidthChars: int
-    thumbnailOffset: int
-    thumbnailSize: int
-    largestExifOffset: int
-    thumbnailAtEnd: char
-    thumbnailSizeOffset: int
-    dateTimeOffsets: int
-    numDateTimeTags: int
-    gpsInfo: GpsInfo
-    qualityGuess: int
+    # fileName*: char
+    # fileDateTime*: DateTime
+    jfifHeader*: JifiHeader
+    # fileSize*: uint
+    # cameraMake*: char
+    # cameraModel*: char
+    # dateTime*: char
+    # height*: uint
+    # width*: uint
+    # orientation*: int
+    # isColor*: int
+    # process*: int
+    # flashUsed*: int
+    # focalLength*: float
+    # exposureTime*: float
+    # apertureFNumer*: float
+    # distance*: float
+    # ccdWidth*: float
+    # exposureBias*: float
+    # digitalZoomRatio*: float
+    # focalLength35mmEquiv*: int
+    # whiteBalance*: int
+    # masteringMode*: int
+    # exposureProgram*: int
+    # exposureMode*:int
+    # isoEquivalent*: int
+    # lightSource*: int
+    # distanceRange*: int
+    # xResolution*: float
+    # yResolution*: float
+    # resolutionUnit*: int
+    # comments*: string
+    # commentWidthChars*: int
+    # thumbnailOffset*: int
+    # thumbnailSize*: int
+    # largestExifOffset*: int
+    # thumbnailAtEnd*: char
+    # thumbnailSizeOffset*: int
+    # dateTimeOffsets*: int
+    # numDateTimeTags*: int
+    # gpsInfo*: GpsInfo
+    # qualityGuess*: int
